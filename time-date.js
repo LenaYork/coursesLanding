c3169b60bc8e1bd16f0180b40faaa5ad5bd3dcdd
Aleksandra <alexandrazvereva7@gmail.com>
2023-06-27 13:30:20 -0500

    feat: stylize landing

--- time-date.js
+++ time-date.js
@@ -1,8 +1,8 @@
-const endDate = new Date("June 14, 2023 12:00:00").getTime();
+const endDate = new Date("June 14, 2024 12:00:00").getTime();
 let timer = setInterval(function () {
     let now = new Date().getTime();
     let t = endDate - now;
-    if (t >= 0) {
+   
 
         let days = Math.floor(t / (1000 * 60 * 60 * 24));
         let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -21,9 +21,6 @@ let timer = setInterval(function () {
         document.getElementById("timer-secs").innerHTML = ("0" + secs).slice(-2) +
             "<span class='label'>seconds</span>";
 
-    } else {
-        document.getElementById("timer").innerHTML = "The countdown is over!";
-
-    }
+    
 
 }, 1000);
