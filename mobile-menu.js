0f83d058d84d70adfc01c2813969025e4dfc01bb
Aleksandra <alexandrazvereva7@gmail.com>
2024-01-22 12:29:01 -0700

    add mobile-menu

---
+++ mobile-menu.js
@@ -0,0 +1,6 @@
+const mobile_menu = document.querySelector(".container-header-mobile");
+document.querySelector(".close-icon").addEventListener("click", () => {
+    console.log("da");
+    mobile_menu.style.display = "none";
+
+})
