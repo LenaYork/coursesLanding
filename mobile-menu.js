486d55f7e1b4ea2a35f8d91e5cbe5254d7d1c054
Aleksandra <alexandrazvereva7@gmail.com>
2024-01-29 13:38:42 -0600

    fix mobile menu

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,6 +1,23 @@
 const mobile_menu = document.querySelector(".container-header-mobile");
-document.querySelector(".close-icon").addEventListener("click", () => {
-    console.log("da");
-    mobile_menu.style.display = "none";
+//document.querySelector(".close-icon").addEventListener("click", () => {
+//   console.log("da");
+//  mobile_menu.style.display = "none";
 
-})
+
+//})
+//document.querySelector(".burger").addEventListener("click", () => {
+//   console.log("da");
+//   mobile_menu.style.display = "flex";
+
+
+//})
+document.querySelector(".close-icon").addEventListener("click", () => {
+    mobile_menu.classList.remove("show")
+}
+);
+document.querySelector(".burger").addEventListener("click", () => {
+    mobile_menu.classList.add("show");
+}
+);
+
+
